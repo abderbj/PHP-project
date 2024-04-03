b9a24b2488e6e53f86bf7639fe64d257a7d3fab3
Ala-ADN <alaadnzaw@gmail.com>
2024-04-03 07:19:55 +0100

    gamous

--- Server/src/signup.php
+++ Server/src/signup.php
@@ -1,13 +1,7 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://localhost:3000");
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-header('Access-Control-Allow-Credentials: true');
-header('Content-Type: application/json');
-
+include 'request.php';
 echo json_encode(array("message"=>"User created hello"));
-// include 'request.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo($_POST['firstName']);
     $requiredFields = ['firstName', 'lastName', 'phoneNumber', 'email', 'password'];
